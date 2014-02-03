4b23aaed2a20bd1cc211ad2f5f537856405c0312
MarkStoner <ms882@york.ac.uk>
2014-02-03 18:36:14 +0000

    Test

--- src/gpigb/analyse/Analyser.java
+++ src/gpigb/analyse/Analyser.java
@@ -5,6 +5,7 @@ import java.util.*;
 
 public interface Analyser
 {
-	public boolean Analyse(RecordSet<?> input);
+
 	public boolean Analyse(List<RecordSet<?>> input);
+	public boolean Analyse(RecordSet<?> input);
 }
