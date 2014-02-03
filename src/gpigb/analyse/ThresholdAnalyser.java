7676d02b74e732723f4cb7d78fc361b00e8b0c58
rd553 <rd553@york.ac.uk>
2014-02-03 15:24:11 +0000

    Threshold analyser implementation

---
+++ src/gpigb/analyse/ThresholdAnalyser.java
@@ -0,0 +1,52 @@
+package gpigb.analyse;
+
+import gpigb.data.*;
+import gpigb.report.Reporter1;
+
+import java.util.*;
+
+public class ThresholdAnalyser implements Analyser{
+	
+	int upperThreshold;
+	int lowerThreshold;
+
+	public ThresholdAnalyser(int u, int l){
+	
+		upperThreshold = u;
+		lowerThreshold = l;
+		
+	}
+	
+
+public boolean analyse(RecordSet<Integer> inputs){
+	
+	int size = inputs.getRecordCount();
+	boolean r = false;
+	
+	for(int i = 0; i<size; i++){
+		if(inputs.getReadingAtPosition(i).getData() < lowerThreshold ||
+			inputs.getReadingAtPosition(i).getData() > upperThreshold){
+			
+			r = true;
+			
+			List<RecordSet<?>> rt = new ArrayList<RecordSet<?>>();
+			RecordSet<Integer> add = new RecordSet<Integer>(inputs.getFromTime(), inputs.getToTime(), inputs.getSensorID());  
+			add.addRecord(inputs.getReadingAtPosition(i));
+			
+			new Reporter1().GenerateReport(rt);
+		
+		
+		}
+		
+		}
+	
+	return r;
+	
+	}
+	
+
+				
+	
+
+
+}
