acfdc6f60a9ffa5001bb05f1d0bbe1cdb4c4bf15
E Ferretti <ef538@pc907s.cs.york.ac.uk>
2014-02-03 13:39:53 +0000

    Some JavaDocs commenting

--- src/gpigb/sense/SensorObserver.java
+++ src/gpigb/sense/SensorObserver.java
@@ -2,5 +2,10 @@ package gpigb.sense;
 
 public interface SensorObserver<DataType>
 {
+	/**
+	 * Returns the new data to the observers after they have been notified
+	 * @param sensorID the ID of the sensor
+	 * @param reading the new data
+	 */
 	public void update(int sensorID, DataType reading);
 }
