4773e3c1b01f1b000486607f9357140d9732752c
rar500 <rar500@york.ac.uk>
2014-05-15 12:43:21 +0100

    Remote management progress

--- src/gpigb/external/HUMSSystem.java
+++ src/gpigb/external/HUMSSystem.java
@@ -4,36 +4,39 @@ import gpigb.analyse.Analyser;
 import gpigb.classloading.ComponentManager.InstanceSummary;
 import gpigb.classloading.ComponentManager.ModuleSummary;
 import gpigb.classloading.StrongReference;
+import gpigb.configuration.ConfigurationValue;
 import gpigb.report.Reporter;
-import gpigb.sense.Sensor;
 import gpigb.store.Store;
 
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
+import java.rmi.Remote;
+import java.rmi.RemoteException;
 import java.util.List;
+import java.util.Map;
 
-public interface HUMSSystem
+public interface HUMSSystem extends Remote
 {
-	public List<ModuleSummary> listAnalyserModules();
-	public int createAnalyser(int moduleID);
-	public List<InstanceSummary> listAnalysers();
-	public StrongReference<Analyser> getAnalyser(int id);
+	public List<ModuleSummary> listAnalyserModules() throws RemoteException;
+	public int createAnalyser(int moduleID) throws RemoteException;
+	public List<InstanceSummary> listAnalysers() throws RemoteException;
+	public StrongReference<Analyser> getAnalyser(int id) throws RemoteException;
 
-	public List<ModuleSummary> listReporterModules();
-	public int createReporter(int moduleID);
-	public List<InstanceSummary> listReporters();
-	public StrongReference<Reporter> getReporter(int id);
+	public List<ModuleSummary> listReporterModules() throws RemoteException;
+	public int createReporter(int moduleID) throws RemoteException;
+	public List<InstanceSummary> listReporters() throws RemoteException;
+	public StrongReference<Reporter> getReporter(int id) throws RemoteException;
 
-	public List<ModuleSummary> listSensorModules();
-	public int createSensor(int moduleID);
-	public List<InstanceSummary> listSensors();
-	public StrongReference<Sensor> getSensor(int id);
+	public List<ModuleSummary> listSensorModules() throws RemoteException;
+	public int createSensor(int moduleID) throws RemoteException;
+	public List<InstanceSummary> listSensors() throws RemoteException;
+	public Map<String, ConfigurationValue> getSensorConfig(int id) throws RemoteException;
+	public boolean setSensorConfig(int id, Map<String, ConfigurationValue> newConfig) throws RemoteException;
 
-	public List<ModuleSummary> listStoreModules();
-	public int createStore(int moduleID);
-	public List<InstanceSummary> listStores();
-	public StrongReference<Store> getStore(int id);
+	public List<ModuleSummary> listStoreModules() throws RemoteException;
+	public int createStore(int moduleID) throws RemoteException;
+	public List<InstanceSummary> listStores() throws RemoteException;
+	public StrongReference<Store> getStore(int id) throws RemoteException;
 	
-	public void uploadJarFile(InputStream uploadStream) throws IOException;
+	public void uploadJarFile(byte[] contents) throws IOException, RemoteException;
 }
--- src/gpigb/testruns/SNMPWithGraphs.java
+++ src/gpigb/testruns/SNMPWithGraphs.java
@@ -1,40 +1,10 @@
 package gpigb.testruns;
 
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.List;
-import java.util.Map;
-
-//import gpigb.analyse.RealTimeGraphAnalyser;
-//import gpigb.analyse.ThresholdAnalyser;
-import gpigb.report.Reporter;
-import gpigb.report.ReporterPlotRTSmart;
-import gpigb.sense.SNMPSensor;
-//import gpigb.report.ReporterPlotRTSmart;
-//import gpigb.report.SimpleWebReporter;
-//import gpigb.sense.SNMPSensor;
-import gpigb.sense.Sensor;
-import gpigb.sense.SensorObserver;
-import gpigb.store.MongoStore;
-//import gpigb.store.InMemoryStore;
-//import gpigb.store.MongoStore;
-import gpigb.store.Store;
-import gpigb.analyse.Analyser;
-import gpigb.analyse.RealTimeGraphAnalyser;
-import gpigb.analyse.ThresholdAnalyser;
-import gpigb.classloading.JarFileComponentManager;
-import gpigb.classloading.StrongReference;
-import gpigb.classloading.ComponentManager.ModuleSummary;
-import gpigb.configuration.ConfigurationHandler;
-import gpigb.configuration.ConfigurationValue;
-import gpigb.configuration.ConfigurationValue.ValueType;
-import gpigb.configuration.handlers.GUIConfigHandler;
-import gpigb.data.SensorRecord;
-import gpigb.data.RecordSet;
+import java.rmi.Naming;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
 import gpigb.external.HUMSSystem;
 import gpigb.external.HUMSSystemImpl;
 
@@ -42,78 +12,25 @@ public class SNMPWithGraphs
 {
 	public static void main(String[] args) throws InterruptedException, FileNotFoundException, IOException
 	{
-		
 		HUMSSystem sys = new HUMSSystemImpl();
-		File f = new File("./HUMS_Modules/test.jar");
-		sys.uploadJarFile(new FileInputStream(f));
 		
-//		JarFileComponentManager<Analyser> aMgr = new JarFileComponentManager<>(Analyser.class);
-//		JarFileComponentManager<Reporter> rMgr = new JarFileComponentManager<>(Reporter.class);
-//		JarFileComponentManager<Sensor> seMgr = new JarFileComponentManager<>(Sensor.class);
-//		JarFileComponentManager<Store> stMgr = new JarFileComponentManager<>(Store.class);
-//		
-//		aMgr.addModuleDirectory("~/HUMS_Modules");
-//		rMgr.addModuleDirectory("~/HUMS_Modules");
-//		seMgr.addModuleDirectory("~/HUMS_Modules");
-//		stMgr.addModuleDirectory("~/HUMS_Modules");
-//		
-//		aMgr.refreshModules();
-//		rMgr.refreshModules();
-//		seMgr.refreshModules();
-//		stMgr.refreshModules();
-//		
-//		Sensor<Float> snmpSensor = new SNMPSensor();
-//		final Store inMemoryStore = new MongoStore();
-//		Analyser thresholdAnalyser = new ThresholdAnalyser();
-//		Analyser realTimeGraphAnalyser = new RealTimeGraphAnalyser();
-//		
-//		ModuleSummary s = rMgr.getAvailableModules().get(0);
-//		rMgr.createObjectOfModule(s.moduleID);
-		
-//		snmpSensor.registerObserver(new SensorObserver<Float>()
-//		{
-//			@Override
-//			public void update(SensorRecord<Float> reading)
-//			{
-//				RecordSet<Float> ds = new RecordSet<>(reading.getTimestamp(), reading.getTimestamp(), reading.getSensorID());
-//				ds.addRecord(reading);
-//				inMemoryStore.write(ds);
-//			}
-//			
-//			@Override
-//			public void update(int sensorID, Float reading)
-//			{
-//				Calendar cal = Calendar.getInstance();
-//				Date time = cal.getTime();
-//				RecordSet<Float> ds = new RecordSet<>(time, time, sensorID);
-//				ds.addRecord(new SensorRecord<Float>(sensorID, reading));
-//				inMemoryStore.write(ds);
-//			}
-//		});
-		
-//		thresholdAnalyser.configure(new GUIConfigHandler(aMgr, rMgr, stMgr, seMgr)); 
-//		ConfigurationHandler()
-//		{
-//			@Override
-//			public void getConfiguration(Map<String, ConfigurationValue> configSpec)
-//			{
-//				configSpec.put("Min", new ConfigurationValue(ValueType.Integer, new Integer(0)));
-//				configSpec.put("Max", new ConfigurationValue(ValueType.Integer, new Integer(32)));
-//			}
-//		});
-		
-//		realTimeGraphAnalyser.configure(new GUIConfigHandler(aMgr, rMgr, stMgr, seMgr));
-		
-//		Reporter webReporter = new SimpleWebReporter();
-//		webReporter.configure(new ConfigurationHandler()
-//		{
-//			
-//			@Override
-//			public void getConfiguration(Map<String, ConfigurationValue> configSpec)
-//			{
-//				configSpec.put("Store", new ConfigurationValue(ValueType.Store, new StrongReference<Store>(inMemoryStore)));
-//			}
-//		});
+		// Open a registry to allow remote access
+		try 
+		{
+            LocateRegistry.createRegistry(1099); 
+        }
+		catch (RemoteException e) 
+		{
+        }
+
+		try
+		{
+			Naming.rebind("HUMS", sys);
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
 		
 		while(true)
 		{
