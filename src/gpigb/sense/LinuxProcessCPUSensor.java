c9f64c075c38935c79b1d4e8e4c60a57b050ddb0
rar500 <rar500@york.ac.uk>
2014-05-16 20:12:50 +0100

    CPU Sensor implementation complete

--- src/gpigb/sense/LinuxProcessCPUSensor.java
+++ src/gpigb/sense/LinuxProcessCPUSensor.java
@@ -20,10 +20,38 @@ public class LinuxProcessCPUSensor implements Sensor<Float>
 {
 	float lastReading = 0;
 	File statFile = null;
-	int pid = 0;
+	int pid = 21794;
 	int cpuID = -1;
 	long oldUtime = -1, oldStime = -1;
 	List<SensorObserver> observers = new ArrayList<>();
+	Float averageUsage = null;
+	Float dampingFactor = 0.5f;
+	
+	public static void main(String[] args)
+	{
+		LinuxProcessCPUSensor c = new LinuxProcessCPUSensor();
+		while(true)
+			try{Thread.sleep(100);}catch(Exception e){}
+	}
+	
+	public LinuxProcessCPUSensor()
+	{
+		new Thread(new Runnable()
+		{
+			@Override
+			public void run()
+			{
+				while(true)
+				{
+					try
+					{
+						Thread.sleep(500);
+					}catch(Exception e){}
+					System.out.println("Reading: " + poll());
+				}
+			}
+		}).start();
+	}
 	
 	@Override
 	public synchronized Map<String, ConfigurationValue> getConfigSpec()
@@ -31,22 +59,6 @@ public class LinuxProcessCPUSensor implements Sensor<Float>
 		HashMap<String, ConfigurationValue> spec = new HashMap<>();
 		spec.put("Process ID", new ConfigurationValue(ValueType.Integer, pid));
 		return spec;
-//		pid = (Integer)spec.get("Process ID").value;
-//		statFile = new File("/proc/" + pid + "/stat");
-//		
-//		try
-//		{
-//			Scanner s = new Scanner(statFile);
-//			String[] contents = s.nextLine().split("\\s");
-//			s.close();
-//			cpuID = Integer.parseInt(contents[38]);
-//			oldStime = Long.parseLong(contents[14]);
-//			oldUtime = Long.parseLong(contents[13]);
-//		}
-//		catch(Exception e)
-//		{
-//			
-//		}
 	}
 	
 	public synchronized boolean setConfig(Map<String, ConfigurationValue> newSpec, ComponentManager<Analyser> aMgr, ComponentManager<Reporter> rMgr, ComponentManager<Sensor> seMgr, ComponentManager<Store> stMgr)
@@ -76,29 +88,67 @@ public class LinuxProcessCPUSensor implements Sensor<Float>
 	    try {
 	        RandomAccessFile reader = new RandomAccessFile("/proc/" + pid + "/stat", "r");
 	        String load = reader.readLine();
-
 	        String[] toks = load.split(" ");
 
-	        long idle1 = Long.parseLong(toks[4]);
-	        long cpu1 = Long.parseLong(toks[2]) + Long.parseLong(toks[3]) + Long.parseLong(toks[5])
+	        long sutime = Long.parseLong(toks[13]);
+	        long sstime = Long.parseLong(toks[14]);
+	        long scutime = Long.parseLong(toks[15]);
+	        long scstime = Long.parseLong(toks[16]);
+	        
+	        String core = toks[38].trim();
+
+	        reader.close();
+	        reader = new RandomAccessFile("/proc/stat", "r");
+	        toks = reader.readLine().split(" ");
+	        reader.close();
+	        long time1 = Long.parseLong(toks[2]) + Long.parseLong(toks[3]) + Long.parseLong(toks[4]) + Long.parseLong(toks[5])
 	              + Long.parseLong(toks[6]) + Long.parseLong(toks[7]) + Long.parseLong(toks[8]);
-
-	        try {
-	            Thread.sleep(360);
-	        } catch (Exception e) {}
-
-	        reader.seek(0);
+	        
+	        try
+	        {
+	        	Thread.sleep(100);
+	        }
+	        catch(Exception e)
+	        {
+	        	
+	        }
+	        
+	        reader.close();
+	        reader = new RandomAccessFile("/proc/" + pid + "/stat", "r");
 	        load = reader.readLine();
 	        reader.close();
-
 	        toks = load.split(" ");
 
-	        long idle2 = Long.parseLong(toks[4]);
-	        long cpu2 = Long.parseLong(toks[2]) + Long.parseLong(toks[3]) + Long.parseLong(toks[5])
-	            + Long.parseLong(toks[6]) + Long.parseLong(toks[7]) + Long.parseLong(toks[8]);
+	        long eutime = Long.parseLong(toks[13]);
+	        long estime = Long.parseLong(toks[14]);
+	        long ecutime = Long.parseLong(toks[15]);
+	        long ecstime = Long.parseLong(toks[16]);
+	        reader.close();
+	        reader = new RandomAccessFile("/proc/stat", "r");
+	        toks = reader.readLine().split(" ");
+	        reader.close();
+	        long time2 = Long.parseLong(toks[2]) + Long.parseLong(toks[3]) + Long.parseLong(toks[4]) + Long.parseLong(toks[5])
+	              + Long.parseLong(toks[6]) + Long.parseLong(toks[7]) + Long.parseLong(toks[8]);
+	        
+	        long dutime = eutime - sutime;
+	        long dstime = estime - sstime;
+	        long dcutime = ecutime - scutime;
+	        long dcstime = ecstime - scstime;
+	        long dt = time2 - time1;
+	        
+	        Float instantaniousUsage = 100*((dutime + dstime + dcutime + dcstime)/(1.0f*dt));
 
-	        return 100f*(cpu2 - cpu1) / ((cpu2 + idle2) - (cpu1 + idle1));
-
+	        if(averageUsage == null)
+	        {
+	        	averageUsage = instantaniousUsage;
+	        }
+	        else
+	        {
+	        	averageUsage = (dampingFactor * averageUsage) + ((1-dampingFactor) * instantaniousUsage);
+	        }
+	        
+	        return averageUsage;
+	        
 	    } catch (IOException ex) {
 	        ex.printStackTrace();
 	    }
